4f0e4e46103cd0540c63a2ee794ccd661d3106a5
benjo247 <benjaminlinge@googlemail.com>
2026-04-19 09:22:04 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
+// PWA Service Worker registrieren
+import { registerSW } from "virtual:pwa-register";
+registerSW({ immediate: true });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);

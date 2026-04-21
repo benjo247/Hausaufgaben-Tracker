99a2eb2e3e6017489c96a3b1411819abb6787489
benjo247 <benjaminlinge@googlemail.com>
2026-04-21 13:08:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Klassly",
-        short_name: "Klassly",
-        theme_color: "#45B7D1",
-        icons: [
-          { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
-        ],
-      },
-    }),
   ],
-  resolve: {
-    alias: {},
-  },
-  optimizeDeps: {
-    include: ["@neondatabase/neon-js"],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
 });
